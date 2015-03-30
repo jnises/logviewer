1cdb93472eb12d46731ad12a163127b1b157d6c8
Joel Nises <joel@nises.se>
2015-03-30 22:22:11 +0200

    more fixes

--- test.rkt
+++ test.rkt
@@ -29,9 +29,9 @@
         (get-visible-position-range start end)
         (list (unbox start) (unbox end))))
     (define (trim-buffer-start)
-      ;; implement
-      #f
-      )
+      (match-let ([(list visible-start _) (get-visible-range)])
+        (let ([trim-end (max 0 (- visible-start trim-margin-char))])
+          (delete 0 trim-end #f))))
     (define (trim-buffer-end)
       (match-let ([(list _ visible-end) (get-visible-range)])
         (let ([trim-start (- (get-end-position) (max 0 (- (get-end-position) visible-end trim-margin-char)))])
@@ -49,32 +49,34 @@
             (let loop ([limit 6])
               (cond
                [(< limit 1) #f]
+               [(eof-object? (peek-byte _file)) #f]
                [(< (peek-byte _file) 128) #f]
                [else (read-byte _file)
                      (loop (- limit 1))]))
             (let* ([newstart (file-position _file)]
-                   [newsize (- refill-size-bytes (- newstart prependstart))]
-                   [data (read-bytes newsize _file)])
+                   [readsize (- start-pos-bytes newstart)]
+                   [data (read-bytes readsize _file)])
               (insert (bytes->string/utf-8 data #\?) 0 'same #f)
-              (set! start-pos-bytes newstart)
-              (when first-update
-                (scroll-to-position (get-end-position))
-                (set! first-update #f)))))
-        (when (> end-char (- buffer-size-char move-margin-char))
+              (set! start-pos-bytes newstart))))
+        (when (> end-char (- (get-end-position) move-margin-char))
           ;; too close to the bottom, try to append more data
           (trim-buffer-start)
-          (let ([appendsize (- buffer-size-char (- end-pos-bytes start-pos-bytes))])
-            ;; read bytes until we find a utf8 start
-                           ;;     [data (let rec ([data (read-bytes newsize _file)]
-                           ;;         [limit 6])
-                           ;; (cond
-                           ;;  [(< limit 1) data]
-                           ;;  [(eof-object? (peek-byte _file)) data]
-                           ;;  [(< (peek-byte _file) 128) data]
-                           ;;  [else (rec (bytes-append data (make-bytes (read-byte _file))) (- limit 1))]))]
-            #f
-            )))
-      (end-edit-sequence))
+          (file-position _file end-pos-bytes)
+          ;; make sure we end at a utf-8 boundary
+          (let ([data (let loop ([limit 6]
+                                 [data (read-bytes refill-size-bytes _file)])
+                        (cond
+                         [(< limit 1) data]
+                         [(eof-object? (peek-byte _file)) data]
+                         [(< (peek-byte _file) 128) data]
+                         [else (loop (- limit 1) (bytes-append data (make-bytes (read-byte _file))))]))])
+            (set! end-pos-bytes (file-position _file))
+            (when (not (eof-object? data))
+              (insert (bytes->string/utf-8 data #\?) 0 'same #f)))))
+      (end-edit-sequence)
+      (when first-update
+        (scroll-to-position (get-end-position))
+        (set! first-update #f)))
     ;; TODO do updates in some smarter way than this
     (set! update-timer (new timer% [notify-callback update]
                             [interval 100]))))
