4a2a7a6602428c27851129c01e9f37a07372875e
Joel Nises <joel@nises.se>
2015-04-03 18:00:53 +0200

    removed possibility to paste things into the editor

--- test.rkt
+++ test.rkt
@@ -110,9 +110,9 @@
 (define t (new logtext% [file (open-input-file "/tmp/testfile")]))
 (send c set-editor t)
 
-(define mb (new menu-bar% [parent frame]))
-(define m-edit (new menu% [label "Edit"] [parent mb]))
-(append-editor-operation-menu-items m-edit #f)
+;; (define mb (new menu-bar% [parent frame]))
+;; (define m-edit (new menu% [label "Edit"] [parent mb]))
+;; (append-editor-operation-menu-items m-edit #f)
 
 (send frame show #t)
 
